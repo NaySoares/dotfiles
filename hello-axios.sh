aad1eb18df8982ac9d9e508f6c533b69ddc9a554
Elienai Soares <elienay.soares07@gmail.com>
2023-07-13 16:42:32 -0300

    add shell script

---
+++ hello-axios.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+
+echo "Hello Axios, starting the setup for your new machine"
+#-----------------------#
+
+#echo "Installing Git"
+#sudo apt-get install git
+
+#echo "Installing ZSH"
+#sudo apt-get install zsh
+
+#echo "Installing Oh My ZSH"
+
